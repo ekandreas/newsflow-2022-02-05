03ee09d2a35a1cf9569a9eaa55e95002ef0e5038
eken-leasify <andreas.ek@leasify.se>
2021-12-19 17:21:30 +0100

    Mix manifest added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <title>Newsflow</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('/dist/css/app.css') }}" rel="stylesheet">
     @livewireStyles
 </head>
 
