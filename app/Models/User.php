4637960e92161700dbfb4c7b08d393434d94dd7e
eken-leasify <andreas.ek@leasify.se>
2021-12-19 18:08:35 +0100

    Everyone should access filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,21 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    public function canAccessFilament(): bool
+    {
+        return true; // str_ends_with($this->email, '@elseif.se') && $this->hasVerifiedEmail();
+    }
 
     /**
      * The attributes that are mass assignable.
